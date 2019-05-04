5c1633fb40058bb24f3a5de3635242a2a4b9eadf
slintristeza <slintristeza@gmail.com>
2019-05-04 16:29:50 +0900

    dev: monster list add button,remove button

--- main.js
+++ main.js
@@ -18,7 +18,8 @@ var app = new Vue({
             { id: 1, name: 'スライム', hp:100 },
             { id: 2, name: 'ゴブリン', hp:200 },
             { id: 3, name: 'ドラゴン', hp:500}
-        ]
+        ],
+        monsterInputName: ""
     },
     methods: {
         handleClick: function(event) {
@@ -26,6 +27,25 @@ var app = new Vue({
         },
         increment: function() {
             this.count ++
+        },
+        // 追加ボタンクリック時のハンドラ
+        doAdd: function() {
+            // リスト内で1番大きいIDを取得
+            var max =this.monsterList.reduce(function(a, b) {
+                return a > b.id ? a : b.id
+            }, 0)
+            // 新しいモンスターをリストに追加
+            this.monsterList.push({
+                id: max + 1,
+                name: this.monsterInputName,
+                hp: 500
+            })
+        },
+        doRemove: function(index) {
+            this.monsterList.splice(index, 1)
+        },
+        changeSlime: function(){
+            this.$set(this.monsterList, 0, { id: 1, name: 'キングスライム', hp: 500 })
         }
     },
     mounted: function() {
