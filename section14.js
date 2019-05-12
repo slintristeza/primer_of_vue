25b0fb2ab388e4e0d1ff201b124131a1c4e67da4
slintristeza <slintristeza@gmail.com>
2019-05-13 07:42:01 +0900

    section14

--- section14.js
+++ section14.js
@@ -1,9 +1,23 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello!'
+        message: 'Hello!',
+        val: "yes",
+        valArray: [],
+        valRadio: '',
+        valSelect: '',
+        valSelectArray: [],
+        preview: '',
+        valRange: 50,
+        price: 100,
+        text: 'moon side'
     },
     methods: {
-
+        handleChange: function(event) {
+            var file = event.target.files[0]
+            if (file && file.type.match('image.*')) {
+                this.preview = window.URL.createObjectURL(file)
+            }
+        }
     }
 })
