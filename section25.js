f2a4a107b3923c5e7dd6e049dbf545994d7af5b3
slintristeza <slintristeza@gmail.com>
2019-05-28 22:22:20 +0900

    section25

--- section25.js
+++ section25.js
@@ -1,6 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
+        name: 'スライム',
+        hp: 100
     },  
     methods: {
     }
---
+++ section25_child.js
@@ -0,0 +1,17 @@
+Vue.component('my-component', {
+    template: '<div class="my-component"><p>名前.{{ name }} HP.{{ hp }}</p><p>名前 <input v-model="localName"></p><p>HP <input size="5" v-model.number="localHp"></p></div>',
+    props: {
+        name: String,
+        hp: Number
+    },
+    computed: {
+        localName: {
+            get: function() { return this.name },
+            set: function(val) { this.$emit('update:name', val)}
+        },
+        localHp: {
+            get: function() { return this.hp },
+            set: function(val) { this.$emit('update:hp', val)}
+        }
+    }
+})
