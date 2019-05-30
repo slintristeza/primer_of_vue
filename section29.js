7733da216449c1ff77878b421f9158c3e9c81930
slintristeza <slintristeza@gmail.com>
2019-05-30 11:20:56 +0900

    section29

---
+++ section29.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        showCat: true,
+        showChar: true,
+        showChar2: true,
+        showChar3: true,
+        count: 0
+    },  
+})
